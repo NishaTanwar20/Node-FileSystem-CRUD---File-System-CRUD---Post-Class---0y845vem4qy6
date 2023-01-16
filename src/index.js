c707328a782cdff9c3f4783931fbca6c978985b5
Nisha Tanwar <103480687+NishaTanwar20@users.noreply.github.com>
2023-01-16 13:54:06 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,22 +3,44 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	try {
+		await fs.writeFile(fileName, fileContent);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	try {
+	const data = await fs.readFile(fileName, 'utf8');
+		console.log(data);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	try {
+		const data = await fs.readFile(fileName, 'utf8');
+		await fs.writeFile(fileName, data + fileContent);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	try {
+		await fs.unlink(fileName);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 
